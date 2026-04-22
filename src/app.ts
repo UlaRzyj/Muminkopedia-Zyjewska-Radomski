f95f67c2a8e2490f32b6ea513863c64aae9cf187
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-04-22 22:12:23 +0200

    connect mongodb

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
@@ -15,5 +16,21 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
     res.json({ message: "API Express + TypeScript działa!" });
 });
+
+const start = async () => {
+    try {
+        await connectDB();
+
+        const PORT = process.env.PORT || 5001;
+
+        app.listen(PORT, () => {
+            console.log(`Serwer działa na porcie ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+start();
 
 export default app;
---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+import moongose from 'mongoose';
+
+export const connectDB = async () => {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+        throw new Error('MONGO_URI is not defined in environment variables');
+    }
+
+    await moongose.connect(uri)
+    console.log("Mongo działa");
+}
